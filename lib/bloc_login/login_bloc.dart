c3c87f0b21c2cc71f4e9a1216b0a8e9d3303e809
josepvazquezp <jose.vazquez@iteso.mx>
2023-11-22 18:30:59 -0600

    login & sigin al 100%

--- lib/bloc_login/login_bloc.dart
+++ lib/bloc_login/login_bloc.dart
@@ -11,352 +11,56 @@ part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
   bool _login = false;
-  Pinwino _user = new Pinwino(
-    id: "123 456 789",
-    nombre: "Pancho Barraza",
-    correo: "caguama@gmail.com",
-    password: "jokeis123",
-    victorias: 0,
-    derrotas: 0,
-    fecha: "28 de Octubre de 2023",
-    conectado: true,
-    deck: [],
-    library: [],
-    gorro: "assets/images/hats/cowboy_hat.webp",
-    gorros: [
-      "assets/images/hats/wizard_hat.webp",
-      "assets/images/hats/cowboy_hat.webp",
-      "assets/images/hats/cap.webp",
-      "assets/images/hats/kirby_hat.png",
-      "assets/images/hats/mario_hat.png",
-      "assets/images/hats/sailor_hat.webp",
-      "assets/images/hats/top_hat.png",
-    ],
-    friends: [
-      "111 222 333",
-      "333 222 111",
-    ],
-  );
+  bool _sigin = false;
+  Pinwino _user = new Pinwino(gorros: [], friends: []);
 
   List<dynamic> _deck = [
-    {
-      "imagen": "assets/images/cards/c1.png",
-      "numero": 3,
-      "color": "blue",
-      "elemento": "fire",
-      "poder": "",
-      "poder_imagen": "",
-    },
-    {
-      "imagen": "assets/images/cards/c2.png",
-      "numero": 6,
-      "color": "purple",
-      "elemento": "fire",
-      "poder": "",
-      "poder_imagen": "",
-    },
-    {
-      "imagen": "assets/images/cards/c3.png",
-      "numero": 2,
-      "color": "yellow",
-      "elemento": "fire",
-      "poder": "",
-      "poder_imagen": ""
-    },
-    {
-      "imagen": "assets/images/cards/c4.png",
-      "numero": 3,
-      "color": "orange",
-      "elemento": "snow",
-      "poder": "",
-      "poder_imagen": "",
-    },
-    {
-      "imagen": "assets/images/cards/c5.png",
-      "numero": 2,
-      "color": "red",
-      "elemento": "snow",
-      "poder": "",
-      "poder_imagen": ""
-    },
-    {
-      "imagen": "assets/images/cards/c6.png",
-      "numero": 7,
-      "color": "yellow",
-      "elemento": "snow",
-      "poder": "",
-      "poder_imagen": "",
-    },
-    {
-      "imagen": "assets/images/cards/c7.png",
-      "numero": 5,
-      "color": "blue",
-      "elemento": "water",
-      "poder": "",
-      "poder_imagen": "",
-    },
-    {
-      "imagen": "assets/images/cards/c8.png",
-      "numero": 2,
-      "color": "green",
-      "elemento": "water",
-      "poder": "",
-      "poder_imagen": "",
-    },
-    {
-      "imagen": "assets/images/cards/c9.png",
-      "numero": 4,
-      "color": "purple",
-      "elemento": "water",
-      "poder": "",
-      "poder_imagen": "",
-    },
-    {
-      "imagen": "assets/images/cards/c10.png",
-      "numero": 10,
-      "color": "yellow",
-      "elemento": "fire",
-      "poder": "1_9",
-      "poder_imagen": "assets/images/elements/1_9_effect.png",
-    },
+    "KYBoJDS6UTyEjyZ2ETAN",
+    "MJRSAvSLFBItkVHGX4Y3",
+    "2BielJ6bafSuIbxF3Pa8",
+    "kRUOW10BAFyaTFK5gl1Z",
+    "cRIqVb0eZYVU0arkDtAC",
+    "iklIRsRuiZy6YWdRlzqW",
+    "nam73FVuji40HacrDHML",
+    "fkhlSJ1L71dbqSQsPjOe",
+    "lTytILCKxZwQAsW6nYw7",
+    "CmX9xHMD9v8oFJovpWQg",
   ];
 
   List<dynamic> _library = [
-    {
-      "imagen": "assets/images/cards/c11.png",
-      "numero": 10,
-      "color": "yellow",
-      "elemento": "water",
-      "poder": "1_9",
-      "poder_imagen": "assets/images/elements/1_9_effect.png",
-    },
-    {
-      "imagen": "assets/images/cards/c12.png",
-      "numero": 10,
-      "color": "yellow",
-      "elemento": "snow",
-      "poder": "1_9",
-      "poder_imagen": "assets/images/elements/1_9_effect.png",
-    },
-    {
-      "imagen": "assets/images/cards/c13.png",
-      "numero": 12,
-      "color": "red",
-      "elemento": "fire",
-      "poder": "+2",
-      "poder_imagen": "assets/images/elements/plus_2.png",
-    },
-    {
-      "imagen": "assets/images/cards/c14.png",
-      "numero": 12,
-      "color": "orange",
-      "elemento": "snow",
-      "poder": "+2",
-      "poder_imagen": "assets/images/elements/plus_2.png",
-    },
-    {
-      "imagen": "assets/images/cards/c15.png",
-      "numero": 12,
-      "color": "purple",
-      "elemento": "water",
-      "poder": "+2",
-      "poder_imagen": "assets/images/elements/plus_2.png",
-    },
-    {
-      "imagen": "assets/images/cards/c16.png",
-      "numero": 12,
-      "color": "blue",
-      "elemento": "fire",
-      "poder": "-2",
-      "poder_imagen": "assets/images/elements/remove_2.png",
-    },
-    {
-      "imagen": "assets/images/cards/c17.png",
-      "numero": 12,
-      "color": "orange",
-      "elemento": "snow",
-      "poder": "-2",
-      "poder_imagen": "assets/images/elements/remove_2.png",
-    },
-    {
-      "imagen": "assets/images/cards/c18.png",
-      "numero": 12,
-      "color": "yellow",
-      "elemento": "water",
-      "poder": "-2",
-      "poder_imagen": "assets/images/elements/remove_2.png",
-    },
-    {
-      "imagen": "assets/images/cards/c19.png",
-      "numero": 10,
-      "color": "orange",
-      "elemento": "fire",
-      "poder": "-snow",
-      "poder_imagen": "assets/images/elements/bye_snow_random.png",
-    },
-    {
-      "imagen": "assets/images/cards/c20.png",
-      "numero": 10,
-      "color": "green",
-      "elemento": "snow",
-      "poder": "-water",
-      "poder_imagen": "assets/images/elements/bye_water_random.png",
-    },
-    {
-      "imagen": "assets/images/cards/c21.png",
-      "numero": 10,
-      "color": "orange",
-      "elemento": "water",
-      "poder": "-fire",
-      "poder_imagen": "assets/images/elements/bye_fire_random.png",
-    },
-    {
-      "imagen": "assets/images/cards/c22.png",
-      "numero": 11,
-      "color": "purple",
-      "elemento": "fire",
-      "poder": "-red",
-      "poder_imagen": "assets/images/elements/bye_red_random.png",
-    },
-    {
-      "imagen": "assets/images/cards/c23.png",
-      "numero": 11,
-      "color": "purple",
-      "elemento": "water",
-      "poder": "-blue",
-      "poder_imagen": "assets/images/elements/bye_blue_random.png",
-    },
-    {
-      "imagen": "assets/images/cards/c24.png",
-      "numero": 11,
-      "color": "purple",
-      "elemento": "snow",
-      "poder": "-green",
-      "poder_imagen": "assets/images/elements/bye_green_random.png",
-    },
-    {
-      "imagen": "assets/images/cards/c25.png",
-      "numero": 11,
-      "color": "green",
-      "elemento": "fire",
-      "poder": "-yellow",
-      "poder_imagen": "assets/images/elements/bye_yellow_random.png",
-    },
-    {
-      "imagen": "assets/images/cards/c26.png",
-      "numero": 11,
-      "color": "green",
-      "elemento": "water",
-      "poder": "-orange",
-      "poder_imagen": "assets/images/elements/bye_orange_random.png",
-    },
-    {
-      "imagen": "assets/images/cards/c27.png",
-      "numero": 11,
-      "color": "yellow",
-      "elemento": "snow",
-      "poder": "-purple",
-      "poder_imagen": "assets/images/elements/bye_purple_random.png",
-    },
-    {
-      "imagen": "assets/images/cards/c28.png",
-      "numero": 11,
-      "color": "blue",
-      "elemento": "fire",
-      "poder": "-all_red",
-      "poder_imagen": "assets/images/elements/bye_all_red.png",
-    },
-    {
-      "imagen": "assets/images/cards/c29.png",
-      "numero": 10,
-      "color": "yellow",
-      "elemento": "fire",
-      "poder": "-all_blue",
-      "poder_imagen": "assets/images/elements/bye_all_blue.png",
-    },
-    {
-      "imagen": "assets/images/cards/c30.png",
-      "numero": 12,
-      "color": "orange",
-      "elemento": "fire",
-      "poder": "-all_green",
-      "poder_imagen": "assets/images/elements/bye_all_green.png",
-    },
-    {
-      "imagen": "assets/images/cards/c31.png",
-      "numero": 10,
-      "color": "purple",
-      "elemento": "water",
-      "poder": "-all_yellow",
-      "poder_imagen": "assets/images/elements/bye_all_yellow.png",
-    },
-    {
-      "imagen": "assets/images/cards/c32.png",
-      "numero": 9,
-      "color": "purple",
-      "elemento": "snow",
-      "poder": "-all_orange",
-      "poder_imagen": "assets/images/elements/bye_all_orange.png",
-    },
-    {
-      "imagen": "assets/images/cards/c33.png",
-      "numero": 10,
-      "color": "green",
-      "elemento": "snow",
-      "poder": "-all_purple",
-      "poder_imagen": "assets/images/elements/bye_all_purple.png",
-    },
-    {
-      "imagen": "assets/images/cards/c34.png",
-      "numero": 9,
-      "color": "red",
-      "elemento": "fire",
-      "poder": "block_snow",
-      "poder_imagen": "assets/images/elements/block_snow.png",
-    },
-    {
-      "imagen": "assets/images/cards/c35.png",
-      "numero": 9,
-      "color": "blue",
-      "elemento": "water",
-      "poder": "block_fire",
-      "poder_imagen": "assets/images/elements/block_fire.png",
-    },
-    {
-      "imagen": "assets/images/cards/c36.png",
-      "numero": 9,
-      "color": "orange",
-      "elemento": "snow",
-      "poder": "block_water",
-      "poder_imagen": "assets/images/elements/block_water.png",
-    },
-    {
-      "imagen": "assets/images/cards/c37.png",
-      "numero": 9,
-      "color": "blue",
-      "elemento": "fire",
-      "poder": "water->fire",
-      "poder_imagen": "assets/images/elements/change_water_fire.png",
-    },
-    {
-      "imagen": "assets/images/cards/c38.png",
-      "numero": 9,
-      "color": "blue",
-      "elemento": "water",
-      "poder": "snow->water",
-      "poder_imagen": "assets/images/elements/change_snow_water.png",
-    },
-    {
-      "imagen": "assets/images/cards/c39.png",
-      "numero": 9,
-      "color": "blue",
-      "elemento": "water",
-      "poder": "fire->snow",
-      "poder_imagen": "assets/images/elements/change_fire_snow.png",
-    },
+    "8MZf4LHiystTmfqNR674",
+    "EyUQ0FOpXRpr9643TcOd",
+    "BMRw71d1LdFscy5sEKGy",
+    "3zvkVVb31WPEAdA6w9ZF",
+    "kMUbqBmSbEtGtiY2rtb3",
+    "CBrHhenE0K7UsZUag4VY",
+    "MF1OG95zjFg64r3sVgsL",
+    "FjYmEVwsGbvv1AtxkfpD",
+    "gyltw2seOhOTfoWolp7S",
+    "J2iUPtxfL7F5Nb1ahh8m",
+    "KDndRPyJJ7FV4nWPCyk4",
+    "baKT99j5DViU4RWjo82Q",
+    "CneI9IiasQG85ayogy3X",
+    "mZmLmOjyzkkxitwh2NXm",
+    "Kbw9BXKv7ISDHPY3BiQ4",
+    "89yCDEs0sVI2siIqZy1u",
+    "C4SDNVzCQmlTOxKVxOLo",
+    "sMMN6vBJj3JHfYhCsfod",
+    "50NegXgt3KzXrxqBm2nV",
+    "JR94rUAcbspQhKbaozOd",
+    "b0OPDHfveXbl1LOyLBVg",
+    "YEtydOd1zZ7ckV9GhOgh",
+    "YCp5ixB1luuCCr70KlY1",
+    "VjrYWn0AFKAZEI3ap1Wv",
+    "2TJO0GiWhNgCxRczbtbw",
+    "pgXlFPe7b6ko4ef35lDJ",
+    "dPbaAYau7FIshAhp2Blv",
+    "NQguYK8EeJi5Gq1mlHO5",
+    "47I7anY3lHX3bJhb21t5",
   ];
 
   bool get getLogin => _login;
+  bool get getSigIn => _sigin;
   Pinwino get getPinwino => _user;
 
   LoginBloc() : super(LoginInitial()) {
@@ -377,9 +81,9 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
           .get();
 
       List<Carta> userDeck =
-          userDeck_Library(userInfo.docs.first.data()["deck"]);
+          await userDeck_Library(userInfo.docs.first.data()["deck"]);
       List<Carta> userLibrary =
-          userDeck_Library(userInfo.docs.first.data()["library"]);
+          await userDeck_Library(userInfo.docs.first.data()["library"]);
 
       _user = new Pinwino(
         id: userInfo.docs.first.id,
@@ -421,7 +125,6 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
           email: event.mail,
           password: event.password,
         );
-        // await credential.user!getIdToken();
 
         // insert en colección
         var newPenwin = {
@@ -449,8 +152,9 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
             .doc(docRef.id)
             .get();
 
-        List<Carta> userDeck = userDeck_Library(userInfo.data()!["deck"]);
-        List<Carta> userLibrary = userDeck_Library(userInfo.data()!["library"]);
+        List<Carta> userDeck = await userDeck_Library(userInfo.data()!["deck"]);
+        List<Carta> userLibrary =
+            await userDeck_Library(userInfo.data()!["library"]);
 
         _user = new Pinwino(
           id: docRef.id,
@@ -469,6 +173,7 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
         );
 
         _login = true;
+        _sigin = true;
         emit(GetUserSuccessState());
       } on FirebaseAuthException catch (e) {
         if (e.code == 'weak-password') {
@@ -476,6 +181,7 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
         } else if (e.code == 'email-already-in-use') {
           print('The account already exists for that email.');
         }
+
         _login = false;
         emit(ErrorState());
       } catch (e) {
@@ -489,18 +195,24 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     }
   }
 
-  List<Carta> userDeck_Library(List<dynamic> list) {
+  Future<List<Carta>> userDeck_Library(List<dynamic> list) async {
     List<Carta> tempList = [];
     Carta temp;
+    var getCard;
 
     for (int i = 0; i < list.length; i++) {
+      getCard = await FirebaseFirestore.instance
+          .collection('cards')
+          .doc(list[i])
+          .get();
+
       temp = new Carta(
-        imagen: list[i]["imagen"],
-        numero: list[i]["numero"],
-        color: list[i]["color"],
-        elemento: list[i]["elemento"],
-        poder: list[i]["poder"],
-        poder_imagen: list[i]["poder_imagen"],
+        imagen: getCard.data()["imagen"],
+        numero: getCard.data()["numero"],
+        color: getCard.data()["color"],
+        elemento: getCard.data()["elemento"],
+        poder: getCard.data()["poder"],
+        poder_imagen: getCard.data()["poder_imagen"],
       );
 
       tempList.add(temp);
